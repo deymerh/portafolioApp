63d6542d55c62a89d6ef1bdad95832fc83fcc1f3
Deymer Hernandez <deymerh@hotmail.com>
2018-04-16 21:32:37 -0500

    Página item portafolio agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
-
+import { ItemPortafolioComponent } from './components/item-portafolio/item-portafolio.component';
 
 
 @NgModule({
@@ -17,6 +17,7 @@ import { AboutComponent } from './components/about/about.component';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
+    ItemPortafolioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/item-portafolio/item-portafolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-portafolio',
+  templateUrl: './item-portafolio.component.html',
+  styles: []
+})
+export class ItemPortafolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
